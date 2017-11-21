ac6431f5846e1c06fde996684e3d14504be30838
abdo38013 <abdo38013@gmail.com>
2017-11-21 21:35:24 +0200

    Create Generic Repository Using C#

---
+++ IRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morphological_generator.EntityFramework.Repositories
+{
+    public interface IRepository<TEntity> where TEntity :class
+    {
+        IEnumerable<TEntity> GetAll(Func<TEntity, bool> predicate = null);
+        TEntity Get(Func<TEntity, bool> predicate);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morphological_generator.EntityFramework.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly MorphologicalContext _dbContext;
+
+        private readonly DbSet<TEntity> DbEntity;
+        public Repository(MorphologicalContext dbContext)
+        {
+            _dbContext = dbContext;
+            DbEntity = _dbContext.Set<TEntity>();
+                
+        }
+        public void Add(TEntity entity)
+        {
+            DbEntity.Add(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            DbEntity.Remove(entity);
+        }
+
+        public TEntity Get(Func<TEntity, bool> predicate)
+        {
+            return DbEntity.FirstOrDefault(predicate);
+        }
+
+        public IEnumerable<TEntity> GetAll(Func<TEntity, bool> predicate = null)
+        {
+            if (predicate != null)
+                return DbEntity.Where(predicate);
+            return DbEntity;
+        }
+
+        public void Update(TEntity entity)
+        {
+            DbEntity.Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
